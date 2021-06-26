a5a2ac13cba06bff4ea4635312c43ba88c0546ae
mahesh <mail.mahesh.lk@gmail.com>
2021-06-26 20:25:14 +0800

    go-client-trino

--- connect/go.mod
+++ connect/go.mod
@@ -1,3 +1,5 @@
 module github.com/mahesh-dilhan/go-trino
 
 go 1.16
+
+require github.com/trinodb/trino-go-client v0.300.0 // indirect
--- connect/main.go
+++ connect/main.go
@@ -6,4 +6,9 @@ import _ "github.com/trinodb/trino-go-client/trino"
 func main() {
 	dsn := "http://user@localhost:8080?catalog=default&schema=test"
 	db, err := sql.Open("trino", dsn)
+	if err != nil {
+		panic("unable to connect")
+	}
+	println(db)
+
 }
